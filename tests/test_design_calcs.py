be19aefafddb35604da4ab8414f9c8f57f1903e5
mdaipayan <49827916+mdaipayan@users.noreply.github.com>
2026-05-16 05:51:53 +0530

    Integrate purlin design and advanced analysis

---
+++ tests/test_design_calcs.py
@@ -0,0 +1,83 @@
+import unittest
+
+from design_calcs import (
+    LoadInputs,
+    ZPurlinASDInputs,
+    ZSectionInputs,
+    code_checks,
+    z_purlin_advanced_analysis,
+    z_purlin_design_moments,
+    z_purlin_flat_width_checks,
+    z_purlin_resolved_loads,
+    z_section_properties,
+)
+
+
+class CodeChecksTest(unittest.TestCase):
+    def test_minimum_depth_check_compares_clear_web_to_required_depth(self):
+        inp = LoadInputs()
+        sec = ZSectionInputs(t_mm=2.5, overall_depth_D_mm=250.0)
+        props = z_section_properties(sec)
+
+        checks = code_checks(
+            inp, sec, props, support_moment_kg_m=0.0, span_moment_kg_m=0.0
+        )
+
+        self.assertAlmostEqual(props["d_clear_mm"], 245.0)
+        self.assertAlmostEqual(checks["minimum_depth_required_mm"], 12.0)
+        self.assertTrue(checks["minimum_depth_check_ok"])
+
+    def test_minimum_depth_check_fails_when_clear_web_is_too_shallow(self):
+        inp = LoadInputs()
+        sec = ZSectionInputs(t_mm=2.5, overall_depth_D_mm=14.0)
+        props = z_section_properties(sec)
+
+        checks = code_checks(
+            inp, sec, props, support_moment_kg_m=0.0, span_moment_kg_m=0.0
+        )
+
+        self.assertLess(props["d_clear_mm"], checks["minimum_depth_required_mm"])
+        self.assertFalse(checks["minimum_depth_check_ok"])
+
+
+class ZPurlinASDWorkflowTest(unittest.TestCase):
+    def test_flat_width_checks_match_preliminary_limits(self):
+        checks = z_purlin_flat_width_checks(ZPurlinASDInputs())
+
+        self.assertAlmostEqual(checks["web_flat_width_mm"], 196.0)
+        self.assertAlmostEqual(checks["web_ratio"], 98.0)
+        self.assertTrue(checks["web_ratio_ok"])
+        self.assertAlmostEqual(checks["flange_flat_width_mm"], 56.0)
+        self.assertAlmostEqual(checks["flange_ratio"], 28.0)
+        self.assertTrue(checks["flange_ratio_ok"])
+
+    def test_load_resolution_and_moments_follow_asd_framework(self):
+        inputs = ZPurlinASDInputs()
+
+        loads = z_purlin_resolved_loads(inputs)
+        moments = z_purlin_design_moments(inputs, loads)
+
+        self.assertAlmostEqual(loads["gravity_line_load_kn_m"], 1.08)
+        self.assertAlmostEqual(loads["gravity_normal_kn_m"], 1.063592, places=6)
+        self.assertAlmostEqual(loads["gravity_tangential_kn_m"], 0.187540, places=6)
+        self.assertAlmostEqual(loads["uplift_line_load_kn_m"], -1.62)
+        self.assertAlmostEqual(moments["gravity_major_axis_kn_m"], 2.658981, places=6)
+        self.assertAlmostEqual(moments["gravity_minor_axis_kn_m"], 0.586063, places=6)
+        self.assertAlmostEqual(moments["uplift_major_axis_kn_m"], -3.988471, places=6)
+
+    def test_advanced_analysis_returns_design_interaction_status(self):
+        inputs = ZPurlinASDInputs()
+        loads = z_purlin_resolved_loads(inputs)
+        moments = z_purlin_design_moments(inputs, loads)
+
+        advanced = z_purlin_advanced_analysis(inputs, moments)
+
+        self.assertGreater(advanced["zxx_effective_cm3"], 0.0)
+        self.assertGreater(advanced["zyy_effective_cm3"], 0.0)
+        self.assertAlmostEqual(advanced["allowable_stress_n_mm2"], 150.0)
+        self.assertIn("gravity_interaction_ok", advanced)
+        self.assertIn("uplift_interaction_ok", advanced)
+
+
+if __name__ == "__main__":
+    unittest.main()
